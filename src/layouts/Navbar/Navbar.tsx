479e9f83708eb66319fe50009f08a621240238cd
hwelsters <corevalue.noel@gmail.com>
2022-11-12 16:20:51 -0700

    extra nav buttons

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,16 +1,24 @@
+import GitHub from "@mui/icons-material/GitHub";
+import LinkedIn from "@mui/icons-material/LinkedIn";
 import pageURLs from "data/pageURLs";
 import { HashLink } from "react-router-hash-link";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <HashLink smooth to="#top" className={styles.root}>
-      <img
-        className={styles.logo}
-        src="https://i.ibb.co/c3FPNch/header-image.png"
-        alt=""
-      />
-      Noel
-    </HashLink>
+    <div className={styles.root}>
+      <HashLink smooth to="#top"  className={styles.section}>
+        <img
+          className={styles.logo}
+          src="https://i.ibb.co/c3FPNch/header-image.png"
+          alt=""
+        />
+        Noel
+      </HashLink>
+      <span className={styles.section}>
+        <a href={pageURLs.linkedInURL}><LinkedIn className={styles.icon}/></a>
+        <a href={pageURLs.githubURL}><GitHub className={styles.icon}/></a>
+      </span>
+    </div>
   );
 }
