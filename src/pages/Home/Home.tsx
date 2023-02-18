b4e08d161076f615cf24b7b3d1521e8a5c9339ed
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-02-17 22:53:59 -0700

    Don't repeat yourself

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,9 +15,7 @@ export default function Home() {
       <Navbar />
       <Header />
       <About/>
-      <Experience/>
       <Projects />
-      <Awards />
       <Contacts />
       <Footer />
     </PageContainer>
